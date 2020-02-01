ca0ea76a1fe3b09d30abf2b240dcd0e46af4cb04
RyanFlachman <ryan@flachman.net>
2020-02-01 11:17:10 -0700

    Add firebase environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBwcFC9_3rsT_-oq12DF_cdxCX3ncyMvYo",
+    authDomain: "shoppingcart-cd72c.firebaseapp.com",
+    databaseURL: "https://shoppingcart-cd72c.firebaseio.com",
+    projectId: "shoppingcart-cd72c",
+    storageBucket: "shoppingcart-cd72c.appspot.com",
+    messagingSenderId: "660024133358",
+    appId: "1:660024133358:web:4ca61ea8e5597e3bcc55ff",
+    measurementId: "G-P0XL8XR1NG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBwcFC9_3rsT_-oq12DF_cdxCX3ncyMvYo",
+    authDomain: "shoppingcart-cd72c.firebaseapp.com",
+    databaseURL: "https://shoppingcart-cd72c.firebaseio.com",
+    projectId: "shoppingcart-cd72c",
+    storageBucket: "shoppingcart-cd72c.appspot.com",
+    messagingSenderId: "660024133358",
+    appId: "1:660024133358:web:4ca61ea8e5597e3bcc55ff",
+    measurementId: "G-P0XL8XR1NG"
+  }
 };
 
 /*
